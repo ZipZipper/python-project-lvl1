0a24653048ea685eb83c281b2f490997ae35f91f
Pavel Bondar <kortofel39@gmail.com>
2023-07-13 21:54:55 +0300

    fixx

--- brain_games/logic/brain_even_game.py
+++ brain_games/logic/brain_even_game.py
@@ -8,9 +8,8 @@ def games():
     num1 = random.randint(1, 100)
     return num1, is_even(num1)
 
-
 def is_even(num_):
-    return num_ % 2 == 0
+    return 'yes' if num_ % 2 == 0 else 'no'
 
 
 def brain_even_game():
