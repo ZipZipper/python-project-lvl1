3e53edb49f58a1c4f66696523a7e720560ca3ff8
Pavel Bondar <kortofel39@gmail.com>
2023-07-13 23:01:44 +0300

    fix10

--- brain_games/games/brain_calc_game.py
+++ brain_games/games/brain_calc_game.py
@@ -1,5 +1,6 @@
 from brain_games.core_logic import start_game
 import random
+import operator
 
 TASK = 'What is the result of the expression?'
 
@@ -10,16 +11,16 @@ def games():
     num2 = random.randint(1, 100)
     result_print = "{0} {1} {2}".format(num1, random_sign, num2)
 
-    return result_print, is_correct_result_exp(num1, random_sign, num2)
+    return result_print, calculate_expression_result(num1, random_sign, num2)
 
 
-def is_correct_result_exp(num1, sing, num2):
-    if sing == "+":
-        return str(num1 + num2)
-    elif sing == "-":
-        return str(num1 - num2)
-    elif sing == "*":
-        return str(num1 * num2)
+def calculate_expression_result(num1, sign, num2):
+    if sign == "+":
+        return str(operator.add(num1, num2))
+    elif sign == "-":
+        return str(operator.sub(num1, num2))
+    elif sign == "*":
+        return str(operator.mul(num1, num2))
 
 
 def brain_calc_game():
