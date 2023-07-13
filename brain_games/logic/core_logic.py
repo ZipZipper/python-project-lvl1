51b5d82115c24d0afaec9e3f1cc2fce76b70b241
Pavel Bondar <kortofel39@gmail.com>
2023-07-13 21:33:56 +0300

    fix

--- brain_games/logic/core_logic.py
+++ brain_games/logic/core_logic.py
@@ -11,12 +11,11 @@ def prompt_and_get_user_name():
     return name
 
 
-def play_rounds(logic, name):
+def play_roundss(logic, name):
     count_win = 0
 
-    ask, resp = logic()
-
     while True:
+        ask, resp = logic()
         print('Question: {}'.format(ask))
         resp_user = prompt.string('Your answer: ')
 
@@ -28,13 +27,13 @@ def play_rounds(logic, name):
                 print('Congratulations, {}!'.format(name))
                 break
         else:
-            print('"{0}" is wrong answer ;(. Correct answer was "{1}"'.format(resp_user, resp))
+            print('"{0}" is wrong answer ;(. Correct answer was "{1}"'.format(
+                resp_user, resp))
             print('Let\'s try again, {}!'.format(name))
-            break
 
 
 def start_game(TASK, logic):
     greetings()
     user_name = prompt_and_get_user_name()
     print(TASK)
-    play_rounds(logic, user_name)
+    play_roundss(logic, user_name)
