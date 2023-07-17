a4203a3d3749259c952a63846630d8fbcd499450
Pavel Bondar <kortofel39@gmail.com>
2023-07-18 02:07:13 +0300

    fix2

--- brain_games/games/brain_even_game.py
+++ brain_games/games/brain_even_game.py
@@ -4,13 +4,18 @@ from brain_games.core_logic import start_game
 TASK = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
+
 def play_games():
     num1 = random.randint(1, 100)
-    return num1, is_even(num1)
+    return num1, bool_to_answer(is_even(num1))
 
 
 def is_even(num_):
     return num_ % 2 == 0
+
+
+def bool_to_answer(value):
+    return "yes" if value else "no"
 
 
 def brain_even_game():
