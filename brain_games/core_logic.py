75e760c9a831b5141e7be4298e0ac72600a65f94
Pavel Bondar <kortofel39@gmail.com>
2023-07-18 01:17:42 +0300

    fix1

--- brain_games/core_logic.py
+++ brain_games/core_logic.py
@@ -11,12 +11,12 @@ def prompt_and_get_user_name():
     return name
 
 
-def play_rounds(ask, name):
+def play_rounds(process, name):
     COUNT_ROUNDS = 3
     count_win = 0
 
     while True:
-        ask, resp = ask()
+        ask, resp = process()
         print('Question: {}'.format(ask))
         resp_user = prompt.string('Your answer: ')
 
