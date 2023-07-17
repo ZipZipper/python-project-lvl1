7545278d1687cf7200df6cad1b30fe28f3f7f2b4
Pavel Bondar <kortofel39@gmail.com>
2023-07-18 02:22:08 +0300

    fix4

--- brain_games/games/brain_gcd_game.py
+++ brain_games/games/brain_gcd_game.py
@@ -6,7 +6,7 @@ TASK = 'Find the greatest common divisor of given numbers.'
 
 def play_games():
     num1, num2 = random.randint(1, 100), random.randint(1, 100)
-    return "{0} {1}".format(num1, num2), int(find_minimal_divisor(num1, num2))
+    return num1, num2, find_minimal_divisor(num1, num2)
 
 
 def find_minimal_divisor(num1, num2):
