af5ca6fb7bd1e47e6827e53f6a6ab228a846f31b
Pavel Bondar <kortofel39@gmail.com>
2023-07-20 05:07:58 +0300

    fix5

--- brain_games/core_logic.py
+++ brain_games/core_logic.py
@@ -1,42 +1,34 @@
 import prompt
+import sys
 
 COUNT_ROUNDS = 3
 
 
-def run_greeting():
+def start_game(game_task, get_game_question_answer):
     print('Welcome to the Brain Games!')
 
+    user_name = prompt.string('May I have your name? ')
+    print(
+        f"Hello, {user_name}!\n"
+        f"{game_task}"
+    )
 
-def prompt_and_get_user_name():
-    name = prompt.string('May I have your name?')
-    print('Hello, {}!'.format(name))
-    return name
+    cycles_count = 3
+    # num_cycles = 3
+    for cycle in range(cycles_count):
+        question, correct_answer = get_game_question_answer()
+        print(f'Question: {question}')
 
-
-def play_rounds(generate_question_and_answer, name):
-    count_win = 0
-
-    while True:
-        question, correct_answer = generate_question_and_answer()
-        print('Question: {}'.format(question))
         user_answer = prompt.string('Your answer: ')
-
         if user_answer == correct_answer:
-            print("Correct!")
-            count_win += 1
+            print('Correct!')
 
-            if count_win == COUNT_ROUNDS:
-                print('Congratulations, {}!'.format(name))
-                break
         else:
-            print('"{0}" is wrong answer ;(. Correct answer was "{1}"'.format(
-                user_answer, correct_answer))
-            print('Let\'s try again, {}!'.format(name))
-            break
+            print(
+                f"'{user_answer}' is wrong answer ;(. "
+                f"Correct answer was '{correct_answer}'.\n"
+                f"Let's try again, {user_name}!"
+            )
+            sys.exit()
 
-
-def start_game(task, logic):
-    run_greeting()
-    user_name = prompt_and_get_user_name()
-    print(task)
-    play_rounds(logic, user_name)
+    print(f'Congratulations, {user_name}!')
