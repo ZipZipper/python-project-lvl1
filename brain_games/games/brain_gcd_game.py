654e258ac38960e29788719c26cc64f9ae750708
Pavel Bondar <kortofel39@gmail.com>
2023-07-20 04:37:24 +0300

    fix3

--- brain_games/games/brain_gcd_game.py
+++ brain_games/games/brain_gcd_game.py
@@ -4,7 +4,7 @@ TASK = 'Find the greatest common divisor of given numbers.'
 
 
 def find_minimal_divisor(num1, num2):
-     for divider in range(min(num1, num2), 0, -1):
+    for divider in range(min(num1, num2), 0, -1):
         if num1 % divider == 0 and num2 % divider == 0:
             return divider
 
